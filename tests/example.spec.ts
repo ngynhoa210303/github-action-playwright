2c56af9b7b20f3d52537761289a95b66f31cbb7e
Nguyen Thi Hoa <hoangyn210303>
2026-01-20 22:01:56 +0700

    clear spec

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,7 +5,7 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
-  // Test1 
+
 });
 
 test('get started link', async ({ page }) => {
